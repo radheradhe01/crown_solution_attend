28ee59d74475ddcf1f655d393453180b4147d657
Bhavesh Varma <your-email@example.com>
2025-12-09 20:22:55 +0530

    updateed

--- Attendance.Wpf/App.xaml.cs
+++ Attendance.Wpf/App.xaml.cs
@@ -1,8 +1,19 @@
 using System.Windows;
+using Attendance.Wpf.Services;
+using System.Linq;
 
 namespace Attendance.Wpf;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        if (!AppServices.Users.GetAll().Any())
+        {
+            AppServices.Auth.CreateUser("admin", "Administrator", "admin123", "Admin");
+        }
+    }
 }
 
